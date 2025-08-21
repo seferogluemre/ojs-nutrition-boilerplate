50f485b68c1baaa0dfaa089cec55d9dcb060b8d3
seferogluemre <yunusemre5331@icloud.com>
2025-08-21 17:37:59 +0300

    fix(errors): correct import name for unauthorized error component

--- apps/web/src/routes/(errors)/401.lazy.tsx
+++ apps/web/src/routes/(errors)/401.lazy.tsx
@@ -1,6 +1,6 @@
-import UnauthorisedError from "#features/errors/unauthorized-error";
+import UnauthorizedError from "#features/errors/unauthorized-error";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/(errors)/401")({
-  component: UnauthorisedError,
+  component: UnauthorizedError,
 });
